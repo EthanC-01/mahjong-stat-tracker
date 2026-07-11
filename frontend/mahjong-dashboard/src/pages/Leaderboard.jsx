25db2b895776302fb8fa5a3ac8d47c5259af0fd5
Johs6652 <ethan2001chen@gmail.com>
2026-07-11 22:52:42 +1000

    added tracking for current win/loss streak

--- frontend/mahjong-dashboard/src/pages/Leaderboard.jsx
+++ frontend/mahjong-dashboard/src/pages/Leaderboard.jsx
@@ -10,15 +10,14 @@ export default function LeaderboardPage({leaderboard}) {
                 <div style={{backgroundColor:"#f1f1f1", height:"100px", borderRadius:"15px", textAlign:"center", color:"black", border: "2px solid #A89B8C", margin:"5px 0 0 0"}}>rank 3</div>
                 */}
                 <span>Rankings</span>
-                <div style={{backgroundColor:"#f1f1f1", height:"300px", borderRadius:"15px", textAlign:"center", color:"black", border: "2px solid #A89B8C", margin:"5px 0 0 0", overflow:"hidden", display:"flex", flexDirection:"column"}}>
-                    <div style={{backgroundColor:"#A89B8C", height:"15%", textAlign:"center", color:"black"}}>header</div>
+                <div style={{backgroundColor:"#f1f1f1", height:"auto", borderRadius:"15px", textAlign:"center", color:"black", border: "2px solid #A89B8C", margin:"5px 0 0 0", overflow:"hidden", display:"flex", flexDirection:"column"}}>
                     <div className="table">
                         {leaderboard.map((player, i) => {
                             const totalGames = player.wins + player.losses + player.draws;
                             const winrate = totalGames === 0 ? (0).toFixed(2) : ((player.wins / totalGames) * 100).toFixed(2);
 
                             return (
-                                <div style={{textAlign:"center", color:"black", height: "35px", border:"1px solid black", fontSize:"14px"}} key={i}>
+                                <div style={{textAlign:"center", color:"black", height: "50px", border:"1px solid black", fontSize:"14px"}} key={i}>
                                     Rank: {player.rank} | Name: {player.players.player_name} | Pts: {player.points} | Wins: {player.wins} | Losses: {player.losses} | Draws: {player.draws} | Winrate: {winrate}%
                                 </div>
                             );
--- frontend/mahjong-dashboard/src/pages/Stats.jsx
+++ frontend/mahjong-dashboard/src/pages/Stats.jsx
@@ -6,7 +6,7 @@ export default function StatsPage({leaderboard}) {
     const [showPlayers, setPlayers] = useState(false);
     const [displayedName, setName] = useState(null);
     const [playerId, setPlayerId] = useState(null);
-    
+    const pstats = ["rank", "wins", "losses", "draws", "longest_win_streak", "longest_loss_streak"]
     useEffect(() => {
         if (leaderboard.length > 0) {
             setName(leaderboard[0]?.players?.player_name);
@@ -42,26 +42,32 @@ export default function StatsPage({leaderboard}) {
                     <button style={{width:"70%", borderRadius:"10px"}} onClick={()=>setPlayers(!showPlayers)}>{displayedName}</button>
                     <button className='nav-tab'>{">"}</button>
                 </div>
-                <div style={{backgroundColor:"#f1f1f1", height:"300px", borderRadius:"15px", textAlign:"center", color:"black", border: "2px solid #A89B8C", margin:"5px 0 0 0", overflow:"hidden", display:"flex", flexDirection:"column"}}>
+                <span>Overview</span>
+                <div style={{height: "70px"}}>
+                    {stats && (
+                        <div style={{color:"black", width:"100%",borderLeftStyle:"none", borderRightStyle:"none", fontSize:"14px", display:"flex", gap: "10px", flexWrap:"wrap", justifyContent:"center"}}>
+                            {pstats.map((stat) => (
+                                <div style={{textAlign:"center", backgroundColor:"#f1f1f1", height: "50px", width:"60px", border: "1px solid black", borderRadius:"5px", display:"flex", flexDirection:"column"}}>
+                                    <span style={{fontSize:"1.5em", fontWeight:"bold"}}>{stats.player_results?.[0]?.[stat]}</span>
+                                    <span style={{fontSize:".8em", color:"grey"}}>{stat.replace(/[_]/g, " ")}</span>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                <span>Match History</span>
+                <div style={{backgroundColor:"#f1f1f1", height:"40px", borderRadius:"15px", border: "2px solid #A89B8C", margin:"5px 0 0 0"}}>
                         {stats && (
-                            <div style={{textAlign:"center", color:"black", height: "35px", width:"100%",borderLeftStyle:"none", borderRightStyle:"none", fontSize:"14px"}}>
-                                Points: {stats.player_results?.[0]?.points} <br/>
-                                Wins: {stats.player_results?.[0]?.wins} <br/>
-                                Losses: {stats.player_results?.[0]?.losses} <br/>
-                                Draws: {stats.player_results?.[0]?.draws} <br/>
-                                Rank: {stats.player_results?.[0]?.rank} <br/>
-                                Win Streak: {stats.player_results?.[0]?.current_win_streak} <br/>
-                                Loss Streak: {stats.player_results?.[0]?.current_loss_streak} <br/>
-                                Longest Win Streak: {stats.player_results?.[0]?.longest_win_streak} <br/>
-                                Longest Loss Streak: {stats.player_results?.[0]?.longest_loss_streak} <br/><br/>
-                                Match History
-                                <div style={{display:"flex", flexDirection:"row", gap:"5px", justifyContent:"center"}}>
+                            <div style={{display:"flex", height:"100%", alignItems:"center", paddingLeft:"10px", paddingRight:"10px", justifyContent:"space-between"}}>
+                                <div style={{display:"flex", flexDirection:"row", gap:"5px"}}>
                                     {stats.match_history?.map((games, i) => (
-                                        <div key={i}>{games.winner ? "win": "loss"}</div>
+                                        <div style={{borderRadius:"1000px", border: "1px solid black", height:"30px", width:"30px", display:"flex", alignItems:"center", justifyContent:"center"}} key={i}>{games.winner ? "W": "L"}</div>
                                     ))}
                                 </div>
+                                <div>{(stats.player_results?.[0]?.current_win_streak) > (stats.player_results?.[0]?.current_loss_streak) ? `↑ ${stats.player_results?.[0]?.current_win_streak} streak` : `↓ ${stats.player_results?.[0]?.current_loss_streak} streak`}</div>
                             </div>
                         )}
+                        
                 </div>
             </div>
 
